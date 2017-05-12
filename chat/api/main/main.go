321ab6207f30592b61b7b2ae696db109eaa9b664
morikuni <taihei.with+git@gmail.com>
2017-05-12 11:19:11 +0900

    Add main for api

---
+++ chat/api/main/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/morikuni/chat/chat/api"
+	"github.com/morikuni/chat/chat/di"
+	"github.com/morikuni/yacm"
+)
+
+func main() {
+	env := yacm.NewEnv().
+		AppendCatcherFuncs(api.Catcher).
+		WithShutterFunc(api.Shutter)
+
+	apis := []api.API{
+		di.NewSignUp(),
+	}
+
+	for _, api := range apis {
+		http.Handle(api.Path(), env.Serve(api))
+	}
+
+	http.ListenAndServe(":8080", nil)
+}
