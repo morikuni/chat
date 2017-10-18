4186955e5f077fbe610e9a569b62fdc2081ff37e
morikuni <taihei.with+git@gmail.com>
2017-10-18 23:17:33 +0900

    Remove impl package

--- src/di/usecase.go
+++ src/di/usecase.go
@@ -2,11 +2,10 @@ package di
 
 import (
 	"github.com/morikuni/chat/src/usecase"
-	"github.com/morikuni/chat/src/usecase/impl"
 )
 
 func InjectPosting() usecase.Posting {
-	return impl.NewPosting(
+	return usecase.NewPosting(
 		InjectChatRepository(),
 	)
 }
--- src/usecase/posting.go
+++ src/usecase/posting.go
@@ -2,8 +2,34 @@ package usecase
 
 import (
 	"context"
+
+	"github.com/morikuni/chat/src/domain/model"
+	"github.com/morikuni/chat/src/domain/repository"
 )
 
 type Posting interface {
 	PostChat(ctx context.Context, message string) error
+}
+
+func NewPosting(chatRepository repository.Chat) Posting {
+	return posting{
+		chatRepository,
+	}
+}
+
+type posting struct {
+	chatRepository repository.Chat
+}
+
+func (p posting) PostChat(ctx context.Context, message string) error {
+	cm, verr := model.ValidateChatMessage(message)
+	if verr != nil {
+		return TranslateValidationError(verr, "message")
+	}
+	id, err := p.chatRepository.GenerateID(ctx)
+	if err != nil {
+		return err
+	}
+	chat := model.NewChat(id, cm)
+	return p.chatRepository.Save(ctx, chat)
 }
