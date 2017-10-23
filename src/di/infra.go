d1fec68fdd9b85cee8947bfae8fd0c38509d1957
morikuni <taihei.with+git@gmail.com>
2017-10-24 00:16:28 +0900

    Remove infra.impl package

--- src/di/infra.go
+++ src/di/infra.go
@@ -1,10 +1,10 @@
 package di
 
 import (
+	"github.com/morikuni/chat/src/adapter/appengine"
 	"github.com/morikuni/chat/src/infra"
-	"github.com/morikuni/chat/src/infra/impl"
 )
 
 func InjectLogger() infra.Logger {
-	return impl.NewLogger()
+	return appengine.NewLogger()
 }
